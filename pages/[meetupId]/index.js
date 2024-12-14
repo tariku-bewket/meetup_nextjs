966ad6a85ebe07b7c8e337a6cccbf9e1255f2a7b
Tariku Bewket <tarikubew@gmail.com>
2024-12-14 12:12:14 +0300

    add custom component and css module for detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 function MeetupDetailPage() {
-  return <h1>Meetup Detail Page</h1>;
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+      title='This is a first meetup'
+      address='Some address 5, 12345 Some City'
+      description='This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!'
+    />
+  );
 }
 
 export default MeetupDetailPage;
