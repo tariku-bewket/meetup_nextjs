e12e9e5dcd10365ee78c1c3e3d986a9b4279a8aa
Tariku Bewket <tarikubew@gmail.com>
2024-12-15 13:26:42 +0300

    server side rendering with getStaticPtops and getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ function MeetupDetailPage() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: 'This is a first meetup',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 5, 12345 Some City',
+        description:
+          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+      },
+    },
+  };
+}
+
 export default MeetupDetailPage;
